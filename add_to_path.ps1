558a7859d31a7b503e1c38a83d790ed7b8dcd2e5
Richard Kovacs <richardkovacs.it@gmail.com>
2020-10-15 15:27:27 +0200

    Add powershell install script

---
+++ add_to_path.ps1
@@ -0,0 +1,9 @@
+[Environment]::GetEnvironmentVariable("Path", "User") > user-path-backup.txt
+echo "User variable PATH has been saved to user-path-backup.txt"
+echo ""
+[Environment]::GetEnvironmentVariable("Path", "Machine") > system-path-backup.txt
+echo "System variable PATH has been saved to system-path-backup.txt"
+echo ""
+[Environment]::SetEnvironmentVariable("Path", [Environment]::GetEnvironmentVariable("Path", "User") + "$pwd", "User")
+echo "Directory of the repository has been added to User variable PATH"
+echo ""
